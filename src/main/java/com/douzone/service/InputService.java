6f4aac39233fd9ffdbe5aaa135c882bf3560ff7d
lmacrown <lmacrown@naver.com>
2023-03-22 17:14:16 +0900

    controller 수정

--- src/main/java/com/douzone/service/InputService.java
+++ src/main/java/com/douzone/service/InputService.java
@@ -36,72 +36,59 @@ public class InputService {
 		return inputDAO.get_tax(params);
 	}
 
-	public Map<String, Object> put_tax(Map<String, Object> params) {
-		boolean is_exist = true;
+	public Map<String, Object> update_taxinfo(Map<String, Object> params) {
 		Map<String, Object> result = new HashMap<>();
-		if (null == params.get("tax_id"))
-			is_exist = false;
-
-		if (!is_exist)
-			tax_insert(params);
-		else {
-			params.put("result", null);
-			String patch_param = (String) params.get("patch_param");// 가져오는 값 확인
-			if (patch_param.equals("payment_date") || patch_param.equals("accrual_ym")) {
-				update_taxdate(params);
-				result.put("earner_tax", null);
-			} else {
-				int i=1;
-				update_taxinfo(params);
-				ResultSet rs = (ResultSet) params.get("result");// 뒤부터 값 확인 코드 => 결과 확인 후 삭제
-				List<Map<String, Object>> earner_taxs  = new ArrayList<>();
-				try {
-					while (rs.next()) {
-						Map<String, Object> earner_tax = new HashMap<String, Object>();
-						EarnerTaxVO earnerTax = new EarnerTaxVO(
-							    rs.getString("is_tuition"),
-							    rs.getString("deduction_amount"),
-							    rs.getString("is_artist"),
-							    rs.getString("ins_reduce"),
-							    rs.getString("tax_id"),
-							    rs.getString("tax_rate"),
-							    rs.getString("ins_rate"),
-							    rs.getString("total_payment"),
-							    rs.getString("accrual_ym"),
-							    rs.getString("payment_ym"),
-							    rs.getString("payment_date"),
-							    rs.getString("calculated"),
-							    rs.getString("tax_income"),
-							    rs.getString("tax_local"),
-							    rs.getString("artist_cost"),
-							    rs.getString("ins_cost"),
-							    rs.getString("tax_total"),
-							    rs.getString("real_payment")
-							);
-						earner_tax.put("earner_tax"+ (i++), earnerTax);
-						earner_taxs.add(earner_tax);
-					}
-					result.put("earner_tax", earner_taxs);
-				} catch (Exception e) {
-					System.out.println(e.getMessage());// 로그로 고칠 것
-				}
+		params.put("result", null);
+			
+		int i=1;
+		inputDAO.update_taxinfo(params);
+		ResultSet rs = (ResultSet) params.get("result");// 뒤부터 값 확인 코드 => 결과 확인 후 삭제
+		List<Map<String, Object>> earner_taxs  = new ArrayList<>();
+		try {
+			while (rs.next()) {
+				Map<String, Object> earner_tax = new HashMap<String, Object>();
+				EarnerTaxVO earnerTax = new EarnerTaxVO(
+					rs.getString("is_tuition"),
+					rs.getString("deduction_amount"),
+					rs.getString("is_artist"),
+					rs.getString("ins_reduce"),
+					rs.getString("tax_id"),
+					rs.getString("tax_rate"),
+					rs.getString("ins_rate"),
+					rs.getString("total_payment"),
+					rs.getString("accrual_ym"),
+					rs.getString("payment_ym"),
+					rs.getString("payment_date"),
+					rs.getString("calculated"),
+					rs.getString("tax_income"),
+					rs.getString("tax_local"),
+					rs.getString("artist_cost"),
+					rs.getString("ins_cost"),
+					rs.getString("tax_total"),
+					rs.getString("real_payment")
+				);
+				earner_tax.put("earner_tax"+ (i++), earnerTax);
+				earner_taxs.add(earner_tax);
 			}
+		result.put("earner_tax", earner_taxs);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());// 로그로 고칠 것
 		}
 		return result;
-
 	}
 
-	private void update_taxdate(Map<String, Object> params) {
+	public void update_taxdate(Map<String, Object> params) {
 		inputDAO.update_taxdate(params);
 	}
 
-	public void tax_insert(Map<String, Object> params) {
+	public int  tax_insert(Map<String, Object> params) {
 		inputDAO.tax_insert(params);
+		return (int)params.get("tax_id");
 	}
 
-	public void update_taxinfo(Map<String, Object> params) {
+//	public void update_taxinfo(Map<String, Object> params) {
 		// inputDAO.tax_update(params);
-		inputDAO.update_taxinfo(params);// 정보 넣고 백업
+//		inputDAO.update_taxinfo(params);// 정보 넣고 백업
 //		System.out.println(params);
 //		 ResultSet rs = (ResultSet)params.get("result");
 //		 System.out.println(rs);
@@ -115,6 +102,6 @@ public class InputService {
 //	        }catch (Exception e ){
 //	            System.out.println(e.getMessage());
 //	        }
-	}
+//	}
 
 }
